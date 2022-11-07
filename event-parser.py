bb0f6b8acd180232ac2f933f082d577ab2170f43
George Sproston <gsproston@scottlogic.com>
2022-11-07 11:54:59 +0000

    Parsing out important XML parts

--- event-parser.py
+++ event-parser.py
@@ -1,4 +1,5 @@
 import sys
+import xml.etree.ElementTree as et
 
 # must have two arguments
 if (len(sys.argv) < 3):
@@ -11,4 +12,28 @@ outputDir = sys.argv[2]
 
 print("Input file: " + inputFile)
 print("Output directory: " + outputDir)
+
+# parse input file
+tree = et.parse(inputFile)
+root = tree.getroot()
+for item in root.findall("./channel/item"):
+  title = item.find("title").text
+  link = item.find("link").text
+  pubDate = item.find("pubDate").text
+  guid = item.find("guid").text
+  eventDate = item.find("events_date").text
+
+  # get all the speakers
+  speakers = []
+  for speaker in item.findall("event_speaker/ul/li"): 
+    speakers.append(speaker.text)
+
+  # parse out the description
+  description = item.find("description").text
+  # remove start and end tags
+  description = description.replace("<![CDATA[", "")
+  description = description.replace("]]>", "")
+  # remove leading and trailing whitespace
+  description = description.strip()
+
 exit(0)
