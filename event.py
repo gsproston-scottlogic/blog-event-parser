dce4362d1efa70e784a2fee015db40fbd36dd290
George Sproston <gsproston@scottlogic.com>
2022-11-07 12:01:37 +0000

    Event class

---
+++ event.py
@@ -0,0 +1,9 @@
+class Event: 
+  def __init__(self, title, link, pubDate, guid, speakers, eventDate, description):
+    self.title = title
+    self.link = link
+    self.pubDate = pubDate
+    self.guid = guid
+    self.speakers = speakers
+    self.eventDate = eventDate
+    self.description = description
